d2ce10d493a58d2ecf3383ef559b75654598eff0
unknown <tannerwlumsden@gmail.com>
2023-01-26 12:13:04 -0500

    changed server.js

--- server.js
+++ server.js
@@ -8,46 +8,28 @@ const db = mysql.createConnection({
     database: "employee_db",
 });
 
-const chooseOption = (type) => {
-    switch (type) {
-        case 'VIEW ALL EMPLOYEES': {
-            db.query('SELECT * FROM employee', (err, employee) => {
-                console.table(employee);
-                init();
-            });
-            break;
+const userPrompt = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'VIEW ALL DEPARTMENTS',
+                'VIEW ALL ROLES',
+                'VIEW ALL EMPLOYEES',
+                'ADD A DEPARTMENT',
+                'ADD A ROLE',
+                'ADD AN EMPLOYEE',
+                'UPDATE AN EMPLOYEE ROLE'
+            ]
         }
-        case 'VIEW ALL DEPARTMENTS': {
-            db.query('SELECT * FROM department', (err, department) => {
-                console.table(department);
-                init();
-            });
-            break;
-        }
-        case 'VIEW ALL ROLES': {
-            db.query('SELECT * FROM role', (err, roles) => {
-                console.table(roles);
-                init();
-            });
-            break;
-        }
-    }
-}
+    ])
+    .then((answers) => {
+        const choices = answers;
 
-const init = () => {
-    prompt({
-        type: 'rawlist',
-        message: 'Choose one of the following categories to view',
-        name: 'type',
-        choices: [
-            'VIEW ALL EMPLOYEES',
-            'VIEW ALL DEPARTMENTS',
-            'VIEW ALL ROLES',
-        ],
+        if (choices === 'VIEW ALL DEPARTMENTS') {
+            showDepartments();
+        }
     })
-        .then((answers) => {
-            chooseOption(answers.type)
-        });
 }
-
-init();
