0f0c206a9d16c94983ca0e8098b84fd908d9440c
unknown <tannerwlumsden@gmail.com>
2023-01-30 19:43:47 -0500

    changed functions

--- server.js
+++ server.js
@@ -6,30 +6,157 @@ require('console.table');
 const db = mysql.createConnection({
     user: "root",
     database: "employee_db",
-});
+},
+    console.log(`Connected to the employee_db`)
+);
 
-const userPrompt = () => {
-    inquirer.prompt ([
+const selectAll = async (table) => {
+    return await db.promise().query('SELECT * FROM ' + table);
+};
+
+const insert = (table, data) => {
+    db.query('INSERT INTO ?? SET ?', [table, data], (err) => {
+        if (err) return console.error(err);
+        console.log('\nSuccessfully created!\n');
+        init();
+    });
+};
+
+const addEmployee = async () => {
+    const [roleData] = await selectAll('role');
+    const [employeeData] = await selectAll('employee');
+    const roles = roleData.map(role => {
+        return {
+            name: role.title,
+            value: role.id
+        }
+    });
+    const managers = employeeData.map(employee => {
+        return {
+            name: employee.first_name + ' ' + employee.last_name,
+            value: employee.id
+        }
+    });
+    const answers = await prompt([
         {
-            type: 'list',
-            name: 'choices',
-            message: 'What would you like to do?',
-            choices: [
-                'VIEW ALL DEPARTMENTS',
-                'VIEW ALL ROLES',
-                'VIEW ALL EMPLOYEES',
-                'ADD A DEPARTMENT',
-                'ADD A ROLE',
-                'ADD AN EMPLOYEE',
-                'UPDATE AN EMPLOYEE ROLE'
-            ]
+            name: 'first_name',
+            message: 'What is the new employee`s first name?'
+        },
+        {
+            name: 'last_name',
+            message: 'What is the new employee`s last name?'
+        },
+        {
+            type: 'rawlist',
+            name: 'role_id',
+            message: 'What is the new employee`s role?',
+            choices: roles,
+        },
+        {
+            type: 'rawlist',
+            name: 'manager_id',
+            message: 'Who is the new employee`s manager?',
+            choices: managers,
         }
     ])
-    .then((answers) => {
-        const choices = answers;
+   
+    insert('employee', answers);
+};
 
-        if (choices === 'VIEW ALL DEPARTMENTS') {
-            showDepartments();
+const addRole = async () => {
+    const [departmentData] = await selectAll('department');
+    const [roleData] = await selectAll('role');
+    const departments = departmentData.map(department => {
+        return {
+            name: department.name,
+            value: department.id
         }
-    })
+    });
+    const answers = await prompt([
+        {
+            name: 'title',
+            message: 'What is the new role`s title?'
+        },
+        {
+            name: 'salary',
+            message: 'What is the salary for the new role?'
+        },
+        {
+            type: 'rawlist',
+            name: 'department_id',
+            message: 'What department does this new role belong to?',
+            choices: departments,
+        },
+    ])
+    
+    insert('role', answers);
+};
+
+const addDepartment = async () => {
+    const answers = await prompt([{
+        name: 'name',
+        message: 'What is the name of this new department?'
+    }
+    ])
+    
+    insert('department', answers);
 }
+
+const chooseOption = async (type) => {
+    switch (type) {
+        case 'View All Employees': {
+            const [data] = await selectAll('employee');
+            console.table(data);
+            init();
+            break;
+        }
+        case 'View All Departments': {
+            const [data] = await selectAll('department');
+            console.table(data);
+            init();
+            break;
+        }
+        case 'View All Roles': {
+            const [data] = await selectAll('role');
+            console.table(data);
+            init();
+            break;
+        }
+        case 'Add Employee': {
+            await addEmployee();
+            init();
+            break;
+        }
+        case 'Add Department': {
+            await addDepartment();
+            init();
+            break;
+        }
+        case 'Add Role': {
+            await addRole();
+            init();
+            break;
+        }
+        
+    }
+}
+
+const init = async () => {
+    const answers = await prompt({
+        type: 'rawlist',
+        message: 'Choose one of the following categories to interact with',
+        choices: [
+            'View All Employees',
+            'View All Departments',
+            'View All Roles',
+            'Add Employee',
+            'Add Department',
+            'Add Role',
+            
+        ],
+        name: 'type',
+    })
+    chooseOption(answers.type)
+}
+
+init();
